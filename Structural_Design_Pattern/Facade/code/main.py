1ce7a41198d773af268177e71fdaa4d350ff67c9
Arghadeep Mazumder <33260290+arghadeep25@users.noreply.github.com>
2025-02-16 14:26:26 +0100

    facade python example added

---
+++ Structural_Design_Pattern/Facade/code/main.py
@@ -0,0 +1,48 @@
+# Internal Process: Video decoder
+class VideoDecoder:
+    def decode(self) -> None:
+        print(f"Decoding Video...")
+
+
+# Internal Process: audio processor
+class AudioProcessor:
+    def process(self) -> None:
+        print(f"Processing audio...")
+
+
+# Internal Process: subtitle loader
+class SubtitleLoader:
+    def load(self) -> None:
+        print(f"Loading subtitles...")
+
+
+# Internal Process: drm checker
+class DRMChecker:
+    def check(self) -> None:
+        print(f"Checking DRM...")
+
+
+# User interface
+class MoviePlayer:
+    def __init__(self) -> None:
+        self.video_decoder: VideoDecoder = VideoDecoder()
+        self.audio_processor: AudioProcessor = AudioProcessor()
+        self.subtitle_loader: SubtitleLoader = SubtitleLoader()
+        self.drm_checker: DRMChecker = DRMChecker()
+
+    def play(self):
+        self.video_decoder.decode()
+        self.audio_processor.process()
+        self.subtitle_loader.load()
+        self.drm_checker.check()
+        print(f"Playing movie...")
+
+
+# Client code
+def main() -> None:
+    movie_player = MoviePlayer()
+    movie_player.play()
+
+
+if __name__ == "__main__":
+    main()
